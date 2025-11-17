da6f4405c68cd296977f671edcc0ab6f01db6f7b
adil <adilkhan49@hotmail.com>
2025-11-17 11:41:00 +0000

    test the kubernetes pod operator

---
+++ dags/kubernetes_pod_operator_test.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+with DAG(
+    dag_id="hello_k8s_pod",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+):
+
+    hello = KubernetesPodOperator(
+        task_id="hello",
+        name="hello-world",
+        namespace="default",
+        image="busybox",
+        cmds=["sh", "-c"],
+        arguments=["echo 'Hello from Kubernetes!'"],
+        get_logs=True,
+    )
