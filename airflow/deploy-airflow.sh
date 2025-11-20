e6a0cb9050279358e66fb35440a2a6c006a319cb
adil <adilkhan49@hotmail.com>
2025-11-20 13:39:43 +0000

    deploy airflow

---
+++ airflow/deploy-airflow.sh
@@ -0,0 +1,7 @@
+export NAMESPACE=airflow
+export RELEASE_NAME=airflow
+
+helm repo add apache-airflow https://airflow.apache.org
+helm repo update
+helm upgrade --install $RELEASE_NAME apache-airflow/airflow --set postgresql.image.tag=latest -f values.yaml -n $NAMESPACE 
+kubectl port-forward svc/$RELEASE_NAME-api-server 8080:8080 -n $NAMESPACE
