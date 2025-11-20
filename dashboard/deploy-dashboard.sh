45c5220d88ab616bb8a8cfbc5bfaa1cd2eae5b4d
adil <adilkhan49@hotmail.com>
2025-11-20 13:13:18 +0000

    deploy dashboard

---
+++ dashboard/deploy-dashboard.sh
@@ -0,0 +1,10 @@
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
+helm uninstall kubernetes-dashboard -n kubernetes-dashboard
+kubeclt delete namespacee kubernetes-dashboard
+helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+kubectl apply -f dashboard-user.yml
+kubectl apply -f dashboard-user-config.yml
+kubectl -n kubernetes-dashboard create token admin-user | pbcopy
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
+
